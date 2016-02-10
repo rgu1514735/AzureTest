b977384c084588f089156eff987cd3a2a860b570
ogunleye <o.d.ogunleye@rgu.ac.uk>
2016-02-10 15:17:34 +0000

    segs

--- get_request.php
+++ get_request.php
@@ -1,7 +1,12 @@
 <?php
     $name = $_POST['name'];
     $email = $_POST['email'];
+    $comment = $_POST['comment'];
+
 
     echo $name;
+    echo "<br>";
     echo $email;
+    echo "<br>";
+    echo $comment;
 ?>
