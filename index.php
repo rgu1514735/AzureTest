6895138fc017a5609e4748e43b72893f5a107ddb
ogunleye <o.d.ogunleye@rgu.ac.uk>
2016-02-24 16:32:54 +0000

    vgh

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <body>
 <form style="margin-left: 300px;" method="post" action="index.php">
     <label> please input a number if it's odd or even</label>
-    <input type="text" max="30" name="num"><br/>
+    <input type="text" max="30" name="num" required><br/>
 
     <input type="submit" value="Check">
 
@@ -13,8 +13,9 @@
 <?php
     $num = $_POST['num'];
     echo $num;
-
-
+    echo "<script type='javascript'>";
+     echo "alert('tobi');";
+    echo "</script>"
 ?>
 
 </body>
