ce0306929aac5cfc0aa718bb213929058bc85d76
ogunleye <o.d.ogunleye@rgu.ac.uk>
2016-02-24 15:14:15 +0000

    vgh

--- index.php
+++ index.php
@@ -4,7 +4,22 @@
 <body>
 <?php
 echo "Hello world";
-echo "tobi ";
+echo "<br/>";
+echo "tobi";
 ?>
+
+<?php
+    $num = 28;
+
+    if($num >10)
+    {
+        echo $num."is greater than 10";
+    }
+    else
+    {
+        echo $num."is lesser than 30";
+    }
+?>
+
 </body>
 </html>
